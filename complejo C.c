69b8386c35ce36413a2399a3743c2f37c3caa298
rodrigo <54129928+rogos01@users.noreply.github.com>
2020-06-20 12:57:45 -0500

    Add files via upload

---
+++ complejo C.c
@@ -0,0 +1,14 @@
+typedef struct complejo complex;
+	struct complejo{
+		float r;					//typedef es para cambiar nombre 
+		float i;
+	}
+complex a, b, c;
+
+
+copmlex suma_complex(complexa, complex b)}
+	complex c;
+	c.r= a.r + b.r;
+	c.i= a.i + b.i;
+	return c;
+}
